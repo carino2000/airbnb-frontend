4916a395dd30fe424a298dc6b7c79d4db75ccbea
dmswl000 <dmszii22@gmail.com>
2025-12-10 18:14:38 +0900

    ㅇ

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+//ㅐㅐㅐㅐ
 
 const router = createBrowserRouter([
   {
